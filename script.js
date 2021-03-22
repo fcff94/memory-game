6415bd6e704713a9f6f79100a92248aba59d2671
fcff94 <fati_carol@hotmail.com>
2021-03-22 21:22:08 +0000

    added html structure to support game; added logic in js;

---
+++ script.js
@@ -0,0 +1,13 @@
+var obj = {
+    x: 1,
+    y: 'carol',
+    z: true,
+    a: null,
+    fn: function(){
+        console.log("hello");
+    },
+    innerObj: {
+        q: 1
+    },
+    arr: [1,2,3,4]
+}
